64ad39c3ba9c83f40ca8f54c5dbf3913ffc48b1f
Facu9922 <95296119+Facu9922@users.noreply.github.com>
2022-05-27 19:09:58 -0300

    cambios de correcion

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 class Usuario {
-    constructor (nombre, apellido){
+    // lo ideal es que el constructor reciba todos los parámetros y no solo nombre y apellido
+    constructor (nombre, apellido, mascotas = [], libros =[]){
         this.nombre = nombre
         this.apellido = apellido
-        this.mascotas = []
-        this.libros = []
+        this.mascotas = mascotas
+        this.libros = libros
+    }
+
+     /* Cuando la consigna pida "devolver", siempre hace referencia a return. El console.log() se podría poner antes pero no es necesario. Además se pedía utilizar "template strings" 
+    
+    Para esta primera función podría ser...
+
+    getFullName(){
+        console.log(`${this.nmbre} ${this.apellido}`) ---> Esta línea no es necesaria, solo sería un extra
+        return `${this.nombre} ${this.apellido}`
     }
     
+    */
+    
     getFullName(){
-        console.log(`Mi nombre es ${this.nombre} ${this.apellido}`)
+        return `${this.nombre} ${this.apellido}`
     }
     addMascotas(...nombre) {
         this.mascotas.push(...nombre)
     }
+
+    // misma aclaración que antes para return vs console.log()
     countMascotas(){
-       console.log (`cantidad de mascotas ${this.mascotas.length}`)
+       return (`cantidad de mascotas ${this.mascotas.length}`)
     }
     addBook(titulo,autor){
         this.libros.push({titulo,autor})
@@ -22,20 +36,19 @@ class Usuario {
         let book=this.libros.map((libro)=>{
             return libro.titulo
             })
-            
-            console.log(`Los titulos de mis libros son: ${book}`)
+            // misma aclaración que antes para return vs console.log()
+            return(`Los titulos de mis libros son: ${book}`)
     }
 }
 const user = new Usuario ("Facundo", "Agudo")
 user.getFullName()
-user.addMascotas("perro", "loro", "gato","Hamster", "Conejo")
+user.addMascotas("Perro", "Loro", "Gato","Hamster", "Conejo")
 
 console.log(user.mascotas)
-user.countMascotas()
+console.log(user.countMascotas())
 user.addBook("Harry Potter", "Rowling")
 user.addBook("1984", "Orwell")
 console.log(user.libros)
 user.getBookNames()
-
 
 
