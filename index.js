ba492cae0c3c1c031219c06d4f92d7034c3ed470
Facu9922 <95296119+Facu9922@users.noreply.github.com>
2022-06-01 14:53:36 -0300

    Add Desafio 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Usuario {
+/* class Usuario {
     // lo ideal es que el constructor reciba todos los parámetros y no solo nombre y apellido
     constructor (nombre, apellido, mascotas = [], libros =[]){
         this.nombre = nombre
@@ -16,7 +16,7 @@ class Usuario {
         return `${this.nombre} ${this.apellido}`
     }
     
-    */
+    
     
     getFullName(){
         return `${this.nombre} ${this.apellido}`
@@ -52,3 +52,109 @@ console.log(user.libros)
 user.getBookNames()
 
 
+ */
+
+
+/* const moment = require("moment")
+
+
+function calcularTiempo(){
+    console.log (moment("19940622", "YYYYMMDD").fromNow())
+    
+    
+}
+calcularTiempo()
+
+var fecha1 = moment('1994-06-22');
+var fecha2 = moment('2022-05-30');
+console.log(fecha2.diff(fecha1, 'year'), ' años han pasado de mi nacimiento ');
+console.log(fecha2.diff(fecha1, 'month'), ' meses han pasado de mi nacimiento');
+console.log(fecha2.diff(fecha1, 'day'), ' dias han pasado de mi nacimiento');
+console.log(fecha2.diff(fecha1, 'hour'), ' horas han pasado de mi nacimiento'); */
+
+
+//Desafio 2
+
+
+const fs = require('fs')
+
+class Contenedor {
+    constructor(fileName) {
+        this.fileName = fileName
+        this.countID = 0
+        this.content = []
+    }
+    async write() {
+        await fs.promises.writeFile(this.fileName, JSON.stringify(this.content))
+    }
+    save(object) {
+        this.countID++ 
+        object["id"] = this.countID 
+        this.content.push(object) 
+        this.write() 
+        return `El id del objeto añadido es ${this.countID}` 
+    }
+    async getAll() { 
+        return this.content
+    }
+
+    getById(id) { 
+        let result
+        if (this.content !== []) {
+            result = this.content.find(x => x.id === id)
+            if (result === undefined) {
+                result = null
+            }
+        } else {
+            result = 'El archivo está vacío'
+        }
+        return result
+    }
+    deleteById(id) { 
+        let result
+        if (this.content !== []) {
+            let newContent = this.content.filter(x => x.id !== id)
+            this.content = newContent
+            this.write() 
+            result = 'OK'
+        } else {
+            result = `El archivo está vacío`
+        }
+        return result
+    }
+
+    async deleteAll() {
+        this.content = this.content.splice(0, this.content.length)
+        this.write()
+    }
+}
+/* const Contenedor =require('./index.js') */
+
+const cargarProds = async function(){
+    let cont= new Contenedor('productos.txt')
+    cont.save({
+        title: "Pelota",
+        price: 150,
+        thumbnail: "https://ep01.epimg.net/verne/imagenes/2015/09/11/articulo/1441988783_165642_1442161238_sumario_normal.jpg"
+    })
+    cont.save({
+        title: "remera",
+        price: 250,
+        thumbnail: "https://d2r9epyceweg5n.cloudfront.net/stores/001/205/102/products/remera-lisa-vi-rj1-122009f0e7fe0bfa3715906956218434-480-0.jpg"
+    })
+    cont.save({
+        title: "celular",
+        price: 500,
+        thumbnail: "https://images.fravega.com/f300/13890bd15f4f552457d38251e9ac7fab.jpg.webp"
+    })
+    console.log(cont.getById(1));
+    console.log(cont.getById(5));
+    console.log(cont.getAll());
+    console.log(cont.deleteById(1));
+    console.log(cont.deleteById(6));
+    console.log(cont.getAll());
+    cont.deleteAll();
+    console.log(cont.getAll());
+}
+
+cargarProds()
