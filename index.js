dcaaf3e2768c51fd444a9124fbea2142d93537fe
Facu9922 <95296119+Facu9922@users.noreply.github.com>
2022-06-08 13:23:38 -0300

    Add desafio 3

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ console.log(fecha2.diff(fecha1, 'hour'), ' horas han pasado de mi nacimiento'); 
 //Desafio 2
 
 
-const fs = require('fs')
+/* const fs = require('fs')
 
 class Contenedor {
     constructor(fileName) {
@@ -127,10 +127,10 @@ class Contenedor {
         this.content = this.content.splice(0, this.content.length)
         this.write()
     }
-}
+} */
 /* const Contenedor =require('./index.js') */
 
-const cargarProds = async function(){
+/* const cargarProds = async function(){
     let cont= new Contenedor('productos.txt')
     cont.save({
         title: "Pelota",
@@ -157,4 +157,68 @@ const cargarProds = async function(){
     console.log(cont.getAll());
 }
 
-cargarProds()
+cargarProds() */
+
+//Desafio 3
+const fs = require('fs');
+
+class Contenedor {
+    constructor(nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    async obtenerTodo() {
+        try {
+            const data = await fs.promises.readFile(`./${this.nombreArchivo}`, 'utf-8');
+            return JSON.parse(data);
+        } catch (error) {
+            return [];
+        }
+    }
+
+    async obtenerPorId(id) {
+        try {
+            const data = await this.obtenerTodo();
+            const resultado = data.find((item) => item.id === id);
+            if (resultado) {
+                return resultado;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.log('Error al obtener un producto por ID: ' + error);
+        }
+    }
+
+}
+const express = require('express');
+
+const app = express();
+
+
+
+const productos = new Contenedor('productos.txt');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="color: blue">Bienvenidos al Servidor Express. Podes ver las rutas /productos y /productoRandom</h1>');
+});
+
+app.get('/productos', async (req, res) => {
+    res.send(await productos.obtenerTodo());
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const data = await productos.obtenerTodo();
+    const numeroRandom = Math.floor(Math.random() * data.length);
+    res.send(await productos.obtenerPorId(parseInt(numeroRandom + 1)));
+});
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto: ${PORT}`);
+});
+
+server.on('error', error => console.log(`Error en el servidor http: ${error}`));
